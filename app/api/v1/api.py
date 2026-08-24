69f47c0c812eee549916742fbc29942f998f967f
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-08-24 23:58:18 +0530

    feat: added retrieval endpoint in the v1 router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1 import documents, health
+from app.api.v1 import documents, health, retrieval
 
 # central router file for all the api v1 routes
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["Healthz"])
-api_router.include_router(documents.router)
+api_router.include_router(documents.router)
+api_router.include_router(retrieval.router)
