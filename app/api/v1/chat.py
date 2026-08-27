ebbda4da51d8d06229a0baa5204c93a0278693a7
Bhavesh Kumar Singh <kishornand.dib@gmail.com>
2026-08-27 12:30:46 +0530

    feat: implemented chat streaming endpoint

--- app/api/v1/chat.py
+++ app/api/v1/chat.py
@@ -10,9 +10,26 @@ router = APIRouter(prefix="/chat", tags=["Chat"])
 @router.post("/ask", response_model = ChatResponse, status_code = status.HTTP_200_OK)
 async def ask_question(
     request: ChatRequest, 
-    chat_serivce: ChatService = Depends(ChatService),
+    chat_service: ChatService = Depends(ChatService),
 ) -> ChatResponse:
-    response = await chat_serivce.generate_chat_response(request)
+    response = await chat_service.generate_chat_response(request)
 
     return response
 
+
+# second endpoint of streaming responses
+@router.post("/completions", status_code = status.HTTP_200_OK)
+async def stream_chat_completions (
+    request: ChatRequest, 
+    chat_service: ChatService = Depends(ChatService),
+) -> StreamingResponse:
+    event_generator = chat_service.generate_chat_response_stream(request)
+    return StreamingResponse(
+        event_generator,
+        media_type = "text/event-stream",
+        headers = {
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+        }
+    )
+    
